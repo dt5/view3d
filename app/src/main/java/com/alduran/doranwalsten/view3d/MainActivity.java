b1d657ae4c3e2e654fe66d1bd32467a3788631fe
Ron Boger <ronboger@gmail.com>
2016-02-14 13:03:55 -0500

    initial commit

---
+++ app/src/main/java/com/alduran/doranwalsten/view3d/MainActivity.java
@@ -0,0 +1,16 @@
+package com.alduran.doranwalsten.view3d;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.startActivity(new Intent(this,Obj3DView.class));
+    }
+}
