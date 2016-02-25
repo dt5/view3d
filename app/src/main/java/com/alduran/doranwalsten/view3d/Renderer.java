443fe9e67b780a69a72eba7d2336841f9362efff
Doran W <doranwalsten@Dorans-MacBook-Air.local>
2016-02-25 08:17:07 -0500

    starting to use OpenGL

--- app/src/main/java/com/alduran/doranwalsten/view3d/Renderer.java
+++ app/src/main/java/com/alduran/doranwalsten/view3d/Renderer.java
@@ -2,34 +2,71 @@ package com.alduran.doranwalsten.view3d;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Point;
+import android.opengl.GLU;
+import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
+import android.view.WindowManager;
 
 import org.rajawali3d.Object3D;
 import org.rajawali3d.lights.DirectionalLight;
 import org.rajawali3d.loader.LoaderOBJ;
 import org.rajawali3d.loader.ParsingException;
 import org.rajawali3d.materials.Material;
+import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.primitives.Sphere;
 import org.rajawali3d.renderer.RajawaliRenderer;
+
 
 /**
  * Created by ron on 2/17/16.
  */
 public class Renderer extends RajawaliRenderer {
+
     public void onTouchEvent(MotionEvent event){
+
+        float winX =  event.getRawX();
+        float winY = event.getRawY();
+
+        Vector3 model_point = screenToWorld(winX,winY,width,height,1);
+        Log.d("locate", String.format("%.2f %.2f %.2f", model_point.x, model_point.y, model_point.z));
     }
 
+    private Vector3 screenToWorld(float x, float y, int viewportWidth, int viewportHeight, float projectionDepth)
+    {
+        float[] r1 = new float[16];
+        int[] viewport = new int[] { 0, 0, viewportWidth, viewportHeight };
+
+        GLU.gluUnProject(x, viewportHeight - y, 0.0f, getCurrentCamera().getViewMatrix().getFloatValues(), 0, getCurrentCamera().getProjectionMatrix().getFloatValues(), 0, viewport, 0, r1, 0);
+
+        //take the normalized vector from the resultant projection and the camera, and then project by the desired distance from the camera.
+        Vector3 result = new Vector3(-r1[0], r1[1], r1[2]);
+        result.subtract(getCurrentCamera().getPosition());
+        result.normalize();
+        result.multiply(projectionDepth);
+        result.add(getCurrentCamera().getPosition());
+        return result;
+    }
     public void onOffsetsChanged(float x, float y, float z, float w, int i, int j){
     }
 
     public Context context;
     public int ID;
-
+    WindowManager wm = (WindowManager) this.context.getSystemService(Context.WINDOW_SERVICE);
+    Display display = wm.getDefaultDisplay();
+    Point dude = new Point();
+    int width;
+    int height;
+    Object3D mObject;
     public Renderer(Context context, int ID) {
         super(context);
         this.context = context;
         setFrameRate(60);
         this.ID = ID;
+        display.getSize(dude);
+        width = dude.x;
+        height = dude.y;
     }
     private DirectionalLight directionalLight;
     private Sphere earthSphere;
@@ -41,7 +78,7 @@ public class Renderer extends RajawaliRenderer {
         directionalLight.setPower(2);
         getCurrentScene().addLight(directionalLight);
 
-        LoaderOBJ objParser = new LoaderOBJ(mContext.getResources(), mTextureManager, R.raw.patrick_obj);
+        LoaderOBJ objParser = new LoaderOBJ(mContext.getResources(), mTextureManager, R.raw.test_obj);
 
 
         try {
@@ -50,7 +87,7 @@ public class Renderer extends RajawaliRenderer {
 
         }
 
-        Object3D mObject = objParser.getParsedObject();
+        mObject = objParser.getParsedObject();
 //      mObject.setColor(Color.TRANSPARENT);
         mObject.setScale(1.f);
         Material material = new Material();
@@ -90,7 +127,7 @@ public class Renderer extends RajawaliRenderer {
 
         arcball.setPosition(0,0,4); //optional
 
-        getCurrentScene().replaceAndSwitchCamera(getCurrentCamera(), arcball);
+        //getCurrentScene().replaceAndSwitchCamera(getCurrentCamera(), arcball);
 
     }
 
